a2d7705fc324852b947d5b8511b62dab5fb4762a
Rich Jones <rich@anomos.info>
2017-12-09 13:39:07 -0500

    organize everything

--- src/RJModules.cpp
+++ src/RJModules.cpp
@@ -11,31 +11,44 @@ void init(rack::Plugin *p) {
 #endif
 	p->website = "https://github.com/Miserlou/RJModules";
 
-	p->addModel(createModel<BitCrushWidget>("RJModules", "BitCrush", "BitCrush", DISTORTION_TAG));
-    p->addModel(createModel<IntegersWidget>("RJModules", "Integers", "Integers", UTILITY_TAG));
-    p->addModel(createModel<FloatsWidget>("RJModules", "Floats", "Floats", UTILITY_TAG));
-    p->addModel(createModel<RandomsWidget>("RJModules", "Randoms", "Randoms", UTILITY_TAG));
-    p->addModel(createModel<ButtonWidget>("RJModules", "Button", "Button", UTILITY_TAG));
-    p->addModel(createModel<ButtonsWidget>("RJModules", "Buttons", "Buttons", UTILITY_TAG));
-    p->addModel(createModel<SplitterWidget>("RJModules", "Splitter", "Splitter", UTILITY_TAG));
-    p->addModel(createModel<SplittersWidget>("RJModules", "Splitters", "Splitters", UTILITY_TAG));
-    p->addModel(createModel<PannerWidget>("RJModules", "Panner", "Panner", UTILITY_TAG));
-    p->addModel(createModel<PannersWidget>("RJModules", "Panners", "Panners", UTILITY_TAG));
-    p->addModel(createModel<FilterWidget>("RJModules", "Filter", "Filter", UTILITY_TAG));
-    p->addModel(createModel<FiltersWidget>("RJModules", "Filters", "Filters", UTILITY_TAG));
-    p->addModel(createModel<NotchWidget>("RJModules", "Notch", "Notch", UTILITY_TAG));
-    p->addModel(createModel<FilterDelayWidget>("RJModules", "FilterDelay", "FilterDelay", DELAY_TAG));
-    p->addModel(createModel<LRMixerWidget>("RJModules", "LRMixer", "LRMixer", UTILITY_TAG));
-    p->addModel(createModel<SupersawWidget>("RJModules", "Supersaw", "Supersaw", LFO_TAG));
-    p->addModel(createModel<TwinLFOWidget>("RJModules", "TwinLFO", "TwinLFO", LFO_TAG));
-    p->addModel(createModel<NoiseWidget>("RJModules", "Noise", "Noise", LFO_TAG));
-    p->addModel(createModel<MonoWidget>("RJModules", "Mono", "Mono", UTILITY_TAG));
-    p->addModel(createModel<VolumesWidget>("RJModules", "Volumes", "Volumes", UTILITY_TAG));
-    p->addModel(createModel<BPMWidget>("RJModules", "BPM", "BPM", UTILITY_TAG));
-    p->addModel(createModel<DisplaysWidget>("RJModules", "Displays", "Displays", UTILITY_TAG));
-    p->addModel(createModel<RangeWidget>("RJModules", "Range", "Range", UTILITY_TAG));
-    p->addModel(createModel<SidechainWidget>("RJModules", "Sidechain", "Sidechain", UTILITY_TAG));
-    p->addModel(createModel<WidenerWidget>("RJModules", "Widener", "Widener", UTILITY_TAG));
+    // Generators
+    p->addModel(createModel<SupersawWidget>("RJModules", "Supersaw", "[GEN] Supersaw", LFO_TAG));
+    p->addModel(createModel<TwinLFOWidget>("RJModules", "TwinLFO", "[GEN] TwinLFO", LFO_TAG));
+    p->addModel(createModel<NoiseWidget>("RJModules", "Noise", "[GEN] Noise", LFO_TAG));
+
+    // FX
+	p->addModel(createModel<BitCrushWidget>("RJModules", "BitCrush", "[FX] BitCrush", DISTORTION_TAG));
+    p->addModel(createModel<WidenerWidget>("RJModules", "Widener", "[FX] Widener", UTILITY_TAG));
+    p->addModel(createModel<FilterDelayWidget>("RJModules", "FilterDelay", "[FX] FilterDelay", DELAY_TAG));
+    p->addModel(createModel<SidechainWidget>("RJModules", "Sidechain", "[FX] Sidechain", UTILITY_TAG));
+
+    // Filters
+    p->addModel(createModel<FilterWidget>("RJModules", "Filter", "[FILT] Filter", UTILITY_TAG));
+    p->addModel(createModel<FiltersWidget>("RJModules", "Filters", "[FILT] Filters", UTILITY_TAG));
+    p->addModel(createModel<NotchWidget>("RJModules", "Notch", "[FILT] Notch", UTILITY_TAG));
+
+    // Numerical
+    p->addModel(createModel<IntegersWidget>("RJModules", "Integers", "[NUM] Integers", UTILITY_TAG));
+    p->addModel(createModel<FloatsWidget>("RJModules", "Floats", "[NUM] Floats", UTILITY_TAG));
+    p->addModel(createModel<RandomsWidget>("RJModules", "Randoms", "[NUM] Randoms", UTILITY_TAG));
+
+    // Mix
+    p->addModel(createModel<LRMixerWidget>("RJModules", "LRMixer", "[MIX] LRMixer", UTILITY_TAG));
+    p->addModel(createModel<MonoWidget>("RJModules", "Mono", "[MIX] Mono", UTILITY_TAG));
+    p->addModel(createModel<VolumesWidget>("RJModules", "Volumes", "[MIX] Volumes", UTILITY_TAG));
+    p->addModel(createModel<PannerWidget>("RJModules", "Panner", "[MIX] Panner", UTILITY_TAG));
+    p->addModel(createModel<PannersWidget>("RJModules", "Panners", "[MIX] Panners", UTILITY_TAG));
+
+    // Live
+    p->addModel(createModel<BPMWidget>("RJModules", "BPM", "[LIVE] BPM", UTILITY_TAG));
+    p->addModel(createModel<ButtonWidget>("RJModules", "Button", "[LIVE] Button", UTILITY_TAG));
+    p->addModel(createModel<ButtonsWidget>("RJModules", "Buttons", "[LIVE] Buttons", UTILITY_TAG));
+
+    // Util
+    p->addModel(createModel<SplitterWidget>("RJModules", "Splitter", "[UTIL] Splitter", UTILITY_TAG));
+    p->addModel(createModel<SplittersWidget>("RJModules", "Splitters", "[UTIL] Splitters", UTILITY_TAG));
+    p->addModel(createModel<DisplaysWidget>("RJModules", "Displays", "[UTIL] Displays", UTILITY_TAG));
+    p->addModel(createModel<RangeWidget>("RJModules", "Range", "[UTIL] Range", UTILITY_TAG));
 
 
 }
