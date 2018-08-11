d41e9e3260ef2117fa0e62052d2fbc1fa981144a
Rich Jones <miserlou@gmail.com>
2018-08-11 15:12:37 -0400

    fix stutter button size

--- make_and_run.sh
+++ make_and_run.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
 
+trash dist
+trash plugins
 RACK_DIR=~/Downloads/Rack-SDK/ make dist
 mkdir -p /Applications/Rack.app/Contents/Resources/plugins
 trash /Applications/Rack.app/Contents/Resources/plugins/RJModules
-cp -r dist/RJModules /Applications/Rack.app/Contents/Resources/plugins/RJModules
+trash ~/Documents/Rack/plugins/RJModules 
+cp -r dist/RJModules /Applications/Rack.app/Contents/Resources/plugins/
+mkdir -p ~/Documents/Rack/plugins/RJModules
+cp -r dist/RJModules ~/Documents/Rack/plugins/
+mkdir -p plugins
+cp -r dist/RJModules plugins/
 /Applications/Rack.app/Contents/MacOS/Rack -d
