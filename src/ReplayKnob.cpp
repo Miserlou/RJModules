4f5699e5f283ebbb1f2f82844ea5b7d8d3f5de07
Rich Jones <rich@anomos.info>
2019-05-23 14:15:15 +0200

    replay button

--- src/ReplayKnob.cpp
+++ src/ReplayKnob.cpp
@@ -17,11 +17,15 @@ struct ReplayKnob : Module {
     enum ParamIds {
         BIG_PARAM,
         REC_PARAM,
+        REPLAY_PARAM,
+        START_PARAM,
+        END_PARAM,
         NUM_PARAMS
     };
     enum InputIds {
         BIG_CV_INPUT,
         REC_CV_INPUT,
+        REPLAY_CV_INPUT,
         NUM_INPUTS
     };
     enum OutputIds {
@@ -30,10 +34,15 @@ struct ReplayKnob : Module {
     };
     enum LightIds {
         REC_LIGHT,
+        REPLAY_LIGHT,
         NUM_LIGHTS
     };
 
     SchmittTrigger recTrigger;
+    SchmittTrigger recTriggerCV;
+    SchmittTrigger replayTrigger;
+    SchmittTrigger replayTriggerCV;
+
     DoubleRingBuffer<float, HISTORY_SIZE> historyBuffer;
     SampleRateConverter<1> src;
 
@@ -42,6 +51,9 @@ struct ReplayKnob : Module {
 
     bool isRecording = false;
     bool hasRecorded = false;
+
+    float replayLight = 0.0;
+    const float lightLambda = 0.075;
 
     ReplayKnob() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
 
@@ -63,9 +75,9 @@ void ReplayKnob::step() {
     outputs[OUT_OUTPUT].value = param;
 
     // Flip the recording state
-    if (recTrigger.process(params[REC_PARAM].value)){
+    if (recTrigger.process(params[REC_PARAM].value) || recTriggerCV.process(inputs[REC_CV_INPUT].value)){
 
-        fprintf( stderr, "I'm recording!" );
+        // fprintf( stderr, "I'm recording!" );
 
         // Clear vector
         if(!isRecording and hasRecorded){
@@ -82,13 +94,31 @@ void ReplayKnob::step() {
 
     if(isRecording){
         replayVector.push_back(param);
-        fprintf( stderr, "I'm recording!" );
+        // fprintf( stderr, "I'm recording!" );
     }
     else if (hasRecorded){
-        fprintf( stderr, "I've recorded!" );
+        // fprintf( stderr, "I've recorded!" );
 
-        if (tapeHead >= replayVector.size()){
-            tapeHead = 0;
+        // Get start and end values
+        float startParam = params[START_PARAM].value;
+        float endParam = params[END_PARAM].value;
+        float vectorSize = replayVector.size();
+        int startPos = (int) (startParam * vectorSize);
+        int endPos = (int) (endParam * vectorSize);
+        if(startPos >= endPos){
+            startPos = endPos;
+        }
+
+        // Are we replaying?
+        if (replayTrigger.process(params[REPLAY_PARAM].value) || replayTriggerCV.process(inputs[REPLAY_CV_INPUT].value)){
+            tapeHead = startPos;
+            replayLight = 1.0;
+        }
+        replayLight -= replayLight / lightLambda / engineGetSampleRate();
+
+        // Loop around
+        if (tapeHead >= endPos || tapeHead >= vectorSize){
+            tapeHead = startPos;
         }
         outputs[OUT_OUTPUT].value = replayVector.at(tapeHead);
         tapeHead++;
@@ -100,6 +130,7 @@ void ReplayKnob::step() {
     } else{
         lights[REC_LIGHT].value = -10.0;
     }
+    lights[REPLAY_LIGHT].value = replayLight;
 
 }
 
@@ -109,6 +140,10 @@ struct ReplayKnobWidget: ModuleWidget {
 };
 
 ReplayKnobWidget::ReplayKnobWidget(ReplayKnob *module) : ModuleWidget(module) {
+
+    float buttonx = 20;
+    float buttony = 114;
+
     box.size = Vec(15*10, 380);
 
     {
@@ -127,12 +162,15 @@ ReplayKnobWidget::ReplayKnobWidget(ReplayKnob *module) : ModuleWidget(module) {
     addInput(Port::create<PJ301MPort>(Vec(17, 50), Port::INPUT, module, ReplayKnob::BIG_CV_INPUT));
     addInput(Port::create<PJ301MPort>(Vec(17, 80), Port::INPUT, module, ReplayKnob::REC_CV_INPUT));
 
-    float buttonx = 20;
-    float buttony = 114;
     addParam(ParamWidget::create<LilLEDButton>(Vec(buttonx, buttony), module, ReplayKnob::REC_PARAM, 0.0, 1.0, 0.0));
     addChild(ModuleLightWidget::create<MediumLight<RedLight>>(Vec(buttonx+4.4, buttony+4.4), module, ReplayKnob::REC_LIGHT));
 
-    // addInput(Port::create<PJ301MPort>(Vec(22, 65), Port::INPUT, module, ReplayKnob::TIME_INPUT));
+    addInput(Port::create<PJ301MPort>(Vec(110, 50), Port::INPUT, module, ReplayKnob::REPLAY_CV_INPUT));
+    addParam(ParamWidget::create<LilLEDButton>(Vec(114, 85), module, ReplayKnob::REPLAY_PARAM, 0.0, 1.0, 0.0));
+    addChild(ModuleLightWidget::create<MediumLight<GreenLight>>(Vec(114+4.4, 85+4.4), module, ReplayKnob::REPLAY_LIGHT));
+
+    addParam(ParamWidget::create<RoundBlackKnob>(Vec(17, 140), module, ReplayKnob::START_PARAM, 0.0, 1.0, 0.0));
+    addParam(ParamWidget::create<RoundBlackKnob>(Vec(58, 140), module, ReplayKnob::END_PARAM, 0.0, 1.0, 1.0));
     addOutput(Port::create<PJ301MPort>(Vec(110, 136), Port::OUTPUT, module, ReplayKnob::OUT_OUTPUT));
 }
 Model *modelReplayKnob = Model::create<ReplayKnob, ReplayKnobWidget>("RJModules", "ReplayKnob", "[LIVE] ReplayKnob", DELAY_TAG);
