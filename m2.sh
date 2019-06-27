6cdec5f174a27caa61f4de5b056c8e08425e2af7
Rich Jones <rich@anomos.info>
2019-06-27 14:32:04 +0200

    oh man
    Former-commit-id: 089a5d2b66cd5c15d77a0ef42c5b444c88494783

--- m2.sh
+++ m2.sh
@@ -4,3 +4,11 @@ trash dist
 trash plugins
 set -e
 RACK_DIR=~/Sources/Rack2/Rack make dist
+
+mkdir -p /Applications/Rack1.app/Contents/Resources/plugins
+cp -r dist/RJModules /Applications/Rack1.app/Contents/Resources/plugins/
+mkdir -p ~/Documents/Rack/plugins-v1/RJModules
+cp -r dist/RJModules ~/Documents/Rack/plugins-v1/
+mkdir -p plugins
+cp -r dist/RJModules plugins/
+/Applications/Rack1.app/Contents/MacOS/Rack -d
