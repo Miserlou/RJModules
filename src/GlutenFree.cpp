861eff58b6224dba4a04bbaeacae1b21d8cf0349
Rich Jones <rich@anomos.info>
2020-02-27 14:12:03 +0100

    GlutenFree only load WAV and wav

--- src/GlutenFree.cpp
+++ src/GlutenFree.cpp
@@ -224,7 +224,9 @@ struct LoadWavButton : SvgSwitch {
         if (module){
             std::string dir = "";
             char *path = osdialog_file(OSDIALOG_OPEN, dir.c_str(), NULL, NULL);
-            if (path) {
+            char* ext;
+            ext = strrchr(path, '.');
+            if (path && ( strcmp(ext, ".wav") == 0 || strcmp(ext, ".WAV") == 0)) {
                 module->loadFile(path);
                 // module->last_path = path;
                 // module->file_chosen = true;
